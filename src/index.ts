b202e762881e1b3c6635c980161a81aba8926ae5
SeregaMW1 <srgrastorguev@gmail.com>
2025-11-01 19:09:49 +0300

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { createApp } from "./app.js";
 import { cfg } from "./config.js";
 import { logger } from "./logger.js";
 import "./mqtt/mqttClient"; 
+import admin from "firebase-admin";
 const app = createApp();
 
 // ✅ Приводим порт к чистому числу (важно)
@@ -11,3 +12,16 @@ const PORT: number = parseInt(process.env.PORT || cfg.PORT?.toString() || "3000"
 app.listen(PORT, "0.0.0.0", () => {
   logger.info(`✅ Server listening on http://0.0.0.0:${PORT}`);
 });
+
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    // Render обычно использует GOOGLE_APPLICATION_CREDENTIALS или переменные окружения
+    // credential: admin.credential.applicationDefault(),
+    // либо:
+    // credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON!)),
+  });
+}
+
+export { admin };
+export const db = admin.firestore();
