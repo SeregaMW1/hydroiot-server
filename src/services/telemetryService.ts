dc14aeb71dc0cfab58713e99f57b51aac6f5a495
SeregaMW1 <srgrastorguev@gmail.com>
2025-11-01 19:25:58 +0300

    Update telemetryService.ts

--- src/services/telemetryService.ts
+++ src/services/telemetryService.ts
@@ -1,6 +1,7 @@
 import { admin, db } from "../firebase";
+import { logger } from "../utils/logger";
 
-type Telemetry = {
+export interface TelemetryData {
   deviceId: string;
   ts?: number;
   ph?: number;
@@ -12,33 +13,43 @@ type Telemetry = {
   levelMax?: boolean;
   rssi?: number;
   fw?: string;
-  receivedAt: Date; // серверное время получения
-};
+  receivedAt: Date;
+}
 
-export async function saveTelemetry(uid: string, deviceId: string, t: Telemetry) {
-  const base = db.collection("users").doc(uid).collection("devices").doc(deviceId);
+/**
+ * Сохраняет телеметрию:
+ * users/{uid}/devices/{deviceId}/telemetry/{autoId}
+ * + обновляет lastTelemetry в документе устройства
+ */
+export async function saveTelemetry(uid: string, deviceId: string, data: TelemetryData) {
+  try {
+    const deviceRef = db.collection("users").doc(uid).collection("devices").doc(deviceId);
 
-  // Добавляем документ в подколлекцию telemetry
-  await base.collection("telemetry").add({
-    ...t,
-    receivedAt: admin.firestore.Timestamp.fromDate(t.receivedAt),
-    serverTs: admin.firestore.FieldValue.serverTimestamp(),
-  });
-
-  // Поддерживаем быстрые поля сводки на устройстве (lastTelemetry)
-  await base.set({
-    lastTelemetry: {
-      ts: t.ts ?? null,
-      ph: t.ph ?? null,
-      ec: t.ec ?? null,
-      waterTempC: t.waterTempC ?? null,
-      airTempC: t.airTempC ?? null,
-      humidity: t.humidity ?? null,
-      levelMin: t.levelMin ?? null,
-      levelMax: t.levelMax ?? null,
-      rssi: t.rssi ?? null,
-      fw: t.fw ?? null,
+    await deviceRef.collection("telemetry").add({
+      ...data,
+      receivedAt: admin.firestore.Timestamp.fromDate(data.receivedAt),
       serverTs: admin.firestore.FieldValue.serverTimestamp(),
-    }
-  }, { merge: true });
+    });
+
+    await deviceRef.set(
+      {
+        lastTelemetry: {
+          ts: data.ts ?? null,
+          ph: data.ph ?? null,
+          ec: data.ec ?? null,
+          waterTempC: data.waterTempC ?? null,
+          airTempC: data.airTempC ?? null,
+          humidity: data.humidity ?? null,
+          levelMin: data.levelMin ?? null,
+          levelMax: data.levelMax ?? null,
+          rssi: data.rssi ?? null,
+          fw: data.fw ?? null,
+          serverTs: admin.firestore.FieldValue.serverTimestamp(),
+        },
+      },
+      { merge: true }
+    );
+  } catch (err: any) {
+    logger.error("[telemetryService] saveTelemetry error", { error: err?.message, deviceId, uid });
+  }
 }
