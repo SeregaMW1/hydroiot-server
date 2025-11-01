67c4d99048df9d99d0cf5f2950c4e56fe399fdef
SeregaMW1 <srgrastorguev@gmail.com>
2025-11-01 19:42:49 +0300

    Update index.ts

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,9 +1,13 @@
+
+
 import admin from "firebase-admin";
 
-// Инициализация Firebase Admin (один раз для всего сервера)
 if (!admin.apps.length) {
   admin.initializeApp();
 }
 
+
 export { admin };
 export const db = admin.firestore();
+
+
