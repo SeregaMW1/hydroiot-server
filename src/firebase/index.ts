254f4931114ffd2cd9f5203f2bc5e2f2896a9671
SeregaMW1 <srgrastorguev@gmail.com>
2025-11-01 20:42:30 +0300

    Update index.ts

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,13 +1,22 @@
-
-
+// src/firebase/index.ts
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  admin.initializeApp();
-}
+  const serviceAccountRaw = process.env.FIREBASE_SERVICE_ACCOUNT;
+  if (!serviceAccountRaw) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT is not set in environment variables");
+  }
 
+  // Преобразуем строку в объект и восстанавливаем переносы ключа
+  const serviceAccount = JSON.parse(serviceAccountRaw);
+  if (serviceAccount.private_key) {
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+  }
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export { admin };
 export const db = admin.firestore();
-
-
